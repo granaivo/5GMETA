54ed0e390b6d0b351187ee9e76c1ac5c0aeb0d29
AMADOU-KOUNTCHE Djibrilla <djibrilla.amadou-kountche@akka.eu>
2025-05-19 21:02:06 +0200

    Refactor examples

--- examples/5gmeta_platform.py
+++ examples/5gmeta_platform.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
 # coding=utf-8
 
-#import os
 from py5gmeta.common.platform import FIVEGMETAPLATFORM
-
-#platform_address = os.environ['CLOUD_PLATFORM_HOST']
-#bootstrap_port = os.environ['KAFKA_BOOTSTRAP_PORT']
-#registry_port = os.environ['REGISTRY_PORT']
-#username =  os.environ['CLOUD_PLATFORM_USERNAME']
-#password = os.environ['CLOUD_PLATFORM_PASSWORD']
-#amqp_user = os.environ['AMQP_USER']
-#amqp_password = os.environ['AMQP_PASSWORD']
-#client_id = os.environ['CLIENT_ID']
-#client_secret = os.environ['CLIENT_SECRET']
-
-
 platform_url = "https://cloudplatform.francecentral.cloudapp.azure.com"
 bootstrap_port = "31081"
 registry_port = "443"
-username =  "test"
-password = "test"
+username =  ""
+password = ""
 amqp_user = "5gmeta-platform"
 amqp_password = "5gmeta-platform"
 realm_name =  "5gmeta"
