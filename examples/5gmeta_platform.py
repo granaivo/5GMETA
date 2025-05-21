da6662be9b7e619233d2224dd89ddc2a2a8cd994
AMADOU-KOUNTCHE Djibrilla <djibrilla.amadou-kountche@akka.eu>
2025-05-21 16:21:08 +0200

    Update the Plaform Python example

--- examples/5gmeta_platform.py
+++ examples/5gmeta_platform.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # coding=utf-8
 
-from py5gmeta.common.platform import FIVEGMETAPLATFORM
+from py5gmeta.common.platform import FiveGMetaPlatform
 platform_url = "https://cloudplatform.francecentral.cloudapp.azure.com"
 bootstrap_port = "31081"
 registry_port = "443"
@@ -13,7 +13,7 @@ realm_name =  "5gmeta"
 client_id = "5gmeta_login"
 client_secret = ""
 
-if __name__ == "__main__":
 
-    platform = FIVEGMETAPLATFORM(platform_url, amqp_user, amqp_password, bootstrap_port, registry_port, realm_name, client_id, client_secret, username, password)
+if __name__ == "__main__":
+    platform = FiveGMetaPlatform(platform_url, amqp_user, amqp_password, bootstrap_port, registry_port, realm_name, client_id, client_secret, username, password)
     platform.run()
