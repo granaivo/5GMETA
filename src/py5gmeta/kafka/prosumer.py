b99e92b75519db87796bba5ca0ef978309be2549
AMADOU-KOUNTCHE Djibrilla <djibrilla.amadou-kountche@akka.eu>
2025-05-21 16:20:48 +0200

    Create a CITS dataset folder

--- src/py5gmeta/kafka/prosumer.py
+++ src/py5gmeta/kafka/prosumer.py
@@ -16,6 +16,13 @@ import requests
 import avro.schema
 from avro.io import DatumReader, BinaryDecoder
 import io
+from uuid import uuid4
+from avro_to_python.reader import AvscReader
+from py5gmeta.common.message import EventMessage
+from py5gmeta.common.helpers import msg_to_dict, delivery_report
+
+import sys
+
 
 gi.require_version('GLib', '2.0')
 gi.require_version('GObject', '2.0')
@@ -227,3 +234,20 @@ def create_consumer(platform_address, bootstrap_port, registry_port, group_id, t
         'key.deserializer': StringDeserializer('utf_8'),
         'value.deserializer': avro_deserializer
     })
+
+
+
+def produce(producer , message, topic):
+    print("Producing EventMessage records to topic {}. ^C to exit.".format(topic))
+    producer.poll(0.0)
+    try:
+        msg_props = {"message": message}
+        msg = EventMessage(properties=msg_props)
+
+        producer.produce(topic=topic, key=str(uuid4()), value=msg, on_delivery=delivery_report)
+
+    except ValueError:
+        print("Invalid input, discarding record...")
+
+    print("\\nFlushing records...")
+    producer.flush()
